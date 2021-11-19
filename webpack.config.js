6aebd8c34a48e6e5408124061aa01a64ef1f86ac
tangtang <1987567948@qq.com>
2021-11-19 15:15:52 +0800

    change:change output config

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,8 @@ module.exports = {
   mode: "production",
   entry: "./index.js",
   output:{
-      // path: path.resolve(__dirname, "dist"),
     //   __dirname指的是项目的根目录
-path:path.resolve(__dirname,'dist'),
-filename:'practice-webpack-yideng.bundle.js',
-
+    path:path.resolve(__dirname,'dist'),
+    filename:'practice-webpack-yideng.bundle.js',
   },
 };
