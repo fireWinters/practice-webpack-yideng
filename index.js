4c8b20623319264884ae6c568db15172261e8482
tangtang <1987567948@qq.com>
2021-11-21 21:10:10 +0800

    change:add webpack-dev-server

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ import img from './src/assets/img/112.png'
 import './src/assets/css/test.less'
 // import './src/assets/css/index.css'
 console.log(img,'img')
+console.log("111img");
+console.log("222img");
+console.log("23332img");   
+    // "serve": "webpack serve",
 list();
