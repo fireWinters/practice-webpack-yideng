f49c123eccc241c8922e3cda2facc916a556c1c1
tangtang <1987567948@qq.com>
2021-11-22 00:05:01 +0800

    change：HMR cancel method

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ console.log("222img");
 console.log("23332img");   
     // "serve": "webpack serve",
 // list();
+// 热更新某个模块
 if (module.hot) {
     module.hot.accept('./list',()=>{
         console.log('热更新模块')
         list();
     })
 }
+// 热更新取消对应模块的写法
+module.hot.decline('./list')
